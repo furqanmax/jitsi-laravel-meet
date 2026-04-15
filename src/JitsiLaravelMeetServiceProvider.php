49906c3f60e83f5e56bf5f796ddd3c62d51eb0aa
furqan <furqan@bizionictech.com>
2026-04-15 17:01:00 +0530

    setvice provider added

---
+++ src/JitsiLaravelMeetServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Furqanamx\JitsiLaravelMeet;
+
+use Illuminate\Support\ServiceProvider;
+use Furqanamx\JitsiLaravelMeet\View\Components\JitsiEmbed;
+
+class JitsiLaravelMeetServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/jitsi.php',
+            'jitsi'
+        );
+    }
+
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'jitsi');
+
+        $this->publishes([
+            __DIR__ . '/../config/jitsi.php' => config_path('jitsi.php'),
+        ], 'jitsi-config');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/jitsi'),
+        ], 'jitsi-views');
+
+        $this->loadRoutesFrom(__DIR__ . '/../routes/jitsi.php');
+
+        $this->loadViewComponentsAs('jitsi', [
+            JitsiEmbed::class,
+        ]);
+    }
+}
